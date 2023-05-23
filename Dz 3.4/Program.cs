c7b48faf7ef11f2f4ca787fb84e634d5b63dedce
ArkadiArh <88594977+ArkadiArh@users.noreply.github.com>
2023-05-23 18:58:06 +0300

    first commit

---
+++ Dz 3.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dz_3._4
+{
+    class Program
+    {
+        /* Переставить элементы массива
+        в обратном порядке (при этом не используя второй массив)*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input arrey size:");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Input {i + 1} element:");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Reverse(array);
+            Console.WriteLine();
+            foreach (var value in array)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
